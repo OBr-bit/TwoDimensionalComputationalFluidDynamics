d0d9d0c70c72a5535a6ac0b65ecf871d8633c7fc
Oliver Browning <OliverBrowningUK@outlook.com>
2026-08-23 16:37:41 +0100

    Changed the grid method into a grid class with the constructor generating the coordinate grids (xv, yv) the velocity grids (u,v) and the pressure grid (p). Also I've ensured that the grid stores the physical step (h) between points and the domain range.

--- grid.py
+++ grid.py
@@ -1,17 +1,16 @@
 import numpy as np
+class Grid:  
+    def __init__ (self, h, firstX, secondX, firstY, secondY):
+        x = np.linspace(firstX, secondX, num=int((secondX-firstX)/h)+1)
+        y = np.linspace(firstY, secondY, num=int((secondY-firstY)/h)+1)
+        self.xv, self.yv = np.meshgrid(x,y)
+        self.u = np.zeros(self.xv.shape)
+        self.v = np.zeros(self.xv.shape)
+        self.p = np.zeros(self.xv.shape)
 
-def GenGrid(h, firstX, secondX, firstY, secondY):
-    x = np.linspace(firstX, secondX, num=int(1/(secondX-firstX)/h)+1)
-    y = np.linspace(firstY, secondY, num=int(1/(secondY-firstY)/h)+1)
-    xv, yv = np.meshgrid(x,y)
-    u = np.zeros(xv.shape)
-    v = np.zeros(xv.shape)
-    p = np.zeros(xv.shape)
-    return xv, yv, u, v, p
-
-xv, yv, u, v, p = GenGrid(0.1, 0, 1, 0, 1)
-print(xv)
-print(yv)
-print(u)
-print(v)
-print(p)
+grid = Grid(0.1, 0, 1, 0, 1)
+print(grid.xv)
+print(grid.yv)
+print(grid.u)
+print(grid.v)
+print(grid.p)
