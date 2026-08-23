a9cf683dd1c154068b4b1c1c3be2868a5821bbf3
Oliver Browning <OliverBrowningUK@outlook.com>
2026-08-23 16:38:57 +0100

    Applied the attribute changes discussed in the previous commit

--- grid.py
+++ grid.py
@@ -1,6 +1,11 @@
 import numpy as np
 class Grid:  
     def __init__ (self, h, firstX, secondX, firstY, secondY):
+        self.h = h
+        self.x_start = firstX
+        self.x_end = secondX
+        self.y_first = firstY
+        self.y_end = secondY
         x = np.linspace(firstX, secondX, num=int((secondX-firstX)/h)+1)
         y = np.linspace(firstY, secondY, num=int((secondY-firstY)/h)+1)
         self.xv, self.yv = np.meshgrid(x,y)
