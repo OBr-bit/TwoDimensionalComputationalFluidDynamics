efd85f1e1ecd5c80ef177a121e2b64b1ffe11e9e
Oliver Browning <OliverBrowningUK@outlook.com>
2026-08-25 18:11:30 +0100

    Developed Boundary.py, this contains the Boundary Conditions class which will have methods to change the behaviour of the fluid and the boundary of the container. Currently they are all set to 0 as default in ApplyBoundaryConditions because following the no-slip condition (where the velocity of the particle adjacent to the wall is 0) the other extremities of the grid has a zero velocity

---
+++ boundary.py
@@ -0,0 +1,11 @@
+class BoundaryConditions:
+    def ApplyBoundaryConditions(self, u, v):
+        u[0, :] = 0
+        u[-1, :] = 0
+        u[:, 0] = 0
+        u[:, -1] = 0
+        v[0, :] = 0
+        v[-1, :] = 0
+        v[:, 0] = 0
+        v[:, -1] = 0
+        return u, v
