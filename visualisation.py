0174a8f5fc83e1dcedaf58a223376c9c68dd9ca3
Oliver Browning <OliverBrowningUK@outlook.com>
2026-08-31 14:59:46 +0100

    Added the centre line velocity plot to the matplotlib display

--- visualisation.py
+++ visualisation.py
@@ -1,3 +1,4 @@
+from matplotlib.lines import lineStyles
 import numpy as np
 import matplotlib.pyplot as plt
 from grid import Grid
@@ -9,15 +10,20 @@ class Visualisation:
 
     def Visualise(self, grid, steps):
         self.fig.clear()
-        flow = self.fig.add_subplot(2, 2, 1)
-        pressure = self.fig.add_subplot(2, 2, 2)
-        conflow = self.fig.add_subplot(2, 2, 3)
-        vorticity = self.fig.add_subplot(2, 2, 4)
+        flow = self.fig.add_subplot(3, 2, 1)
+        pressure = self.fig.add_subplot(3, 2, 2)
+        conflow = self.fig.add_subplot(3, 2, 3)
+        vorticity = self.fig.add_subplot(3, 2, 4)
+        velocity_profile = self.fig.add_subplot(3,2,5)
         flow.set_title(f"Step {steps}")
         flow.quiver(grid.xv, grid.yv, grid.u, grid.v)
         conflow.streamplot(grid.xv[0, :], grid.yv[:, 0], grid.u, grid.v)
         pressure_plot = pressure.contourf(grid.xv, grid.yv, grid.p)
         vorticity_plot = vorticity.contourf(grid.xv, grid.yv, grid.vorticity)
+        velocity_profile.plot(grid.u[:,int(grid.u.shape[1] / 2)], grid.yv[:, 0])
+        velocity_profile.axvline(x=0, color='grey', linestyle='--')
+        velocity_profile.set_xlabel('u velocity')
+        velocity_profile.set_ylabel('y')
         self.fig.colorbar(pressure_plot, ax=pressure)
         self.fig.colorbar(vorticity_plot, ax=vorticity)
         self.fig.tight_layout()
